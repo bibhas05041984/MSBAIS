72ee81c456ee1a7c87a2d70b2f7116cb4ff2322b
bibhas05041984 <bibhas.apr5@gmail.com>
2020-06-20 00:21:38 -0400

    Initial check-in of module MyFirstProject

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace My_first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output of text to the user
+            Console.WriteLine("Let me try again in C#.");
+
+            // Output of a number to the user
+            Console.WriteLine(37);
+
+            Console.WriteLine(1 + 1);
+
+            Console.WriteLine((1 + 2) * 3);
+
+            // Waiting for Enter
+
+            //Console.ReadLine();
+           
+        }
+    }
+}
